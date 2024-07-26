866ca6f14e9af7e40fa29bb4d50f34d544e90143
Harsha <harshathota072@gmail.com>
2024-07-26 22:53:19 +0530

    simple_login

---
+++ vs/simple_login_script.js
@@ -0,0 +1,23 @@
+document.getElementById("openPopupBtn").addEventListener("click", function() {
+    document.getElementById("popupForm").style.display = "flex";
+});
+
+document.getElementById("closePopupBtn").addEventListener("click", function() {
+    document.getElementById("popupForm").style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+    var popup = document.getElementById("popupForm");
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+});
+
+function togglePassword() {
+    var passwordField = document.getElementById("password");
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+    } else {
+        passwordField.type = "password";
+    }
+}
